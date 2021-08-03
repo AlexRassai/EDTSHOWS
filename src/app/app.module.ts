0727f0091a2fe3e4adcfb778edbd83f070c03944
Ali Rassai <helpdesk@koolak.ca>
2021-08-02 20:43:51 -0400

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/ui/layout/layout.component';
+import { NavbarComponent } from './components/ui/navbar/navbar.component';
+import { HeaderComponent } from './components/ui/header/header.component';
+import { SliderComponent } from './components/ui/slider/slider.component';
+import { FooterComponent } from './components/ui/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    NavbarComponent,
+    HeaderComponent,
+    SliderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
